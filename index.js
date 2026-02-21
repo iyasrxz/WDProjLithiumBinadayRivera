36e525df2af45fd961df4b000ef4ad6911cd1acc
bririvera11 <b2029babrivera@pshs.edu.ph>
2026-02-21 13:21:26 +0800

    paiyak na ko

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const images = document.querySelectorAll('.animate');
+
+const obeserve = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }   
+    });
+});
+
+images.forEach(image => {
+    obeserve.observe(image);
+}
+);
 
