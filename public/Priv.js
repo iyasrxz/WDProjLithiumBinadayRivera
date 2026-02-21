2a47d2aa484adf75dbdfbe0a79484e10b8b8c360
bririvera11 <b2029babrivera@pshs.edu.ph>
2026-02-21 23:37:06 +0800

    LOCKED IN

--- public/Priv.js
+++ public/Priv.js
@@ -2,7 +2,7 @@ function privatePage() {
     const currentAccount = JSON.parse(localStorage.getItem("currentAccount"));
     if (!currentAccount) {
         alert("You must be logged in to access this page.");
-        window.location.href = "../public/SignUp.html";
+        window.location.href = "../index.html";
         return;
     }
 }
