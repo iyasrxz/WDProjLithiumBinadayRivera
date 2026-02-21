ca6366c5f8b1a0f656b7183dc4c248ea92c4ff78
iyasrxz <b2029albbinaday@pshs.edu.ph>
2026-02-22 04:08:17 +0800

    haha di parin tapos...

--- index.js
+++ index.js
@@ -121,3 +121,12 @@ setInterval(() => {
 
 animate();
             
+window.addEventListener("DOMContentLoaded", () => {
+    const saved = localStorage.getItem("currentAccount");
+    if (!saved) return;
+
+    const account = JSON.parse(saved);
+
+    // apply saved theme on page load
+    document.body.className = account.activeTheme || "default";
+    });
